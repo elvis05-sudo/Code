c34dafa35c115299bba57fa3e800a3ea6002934e
Elvis <elvisngugi26@gmail.com>
2024-12-31 13:14:50 +0300

    my files

---
+++ script.js
@@ -0,0 +1,51 @@
+// function form(){
+//     let name = document.forms["AdminForm"]["fname"].value;
+//     let age = document.forms["AdminForm"]["ageIn"].value;
+//     let prevSchool = document.forms["AdminForm"]["prevSchoolIn"].value;
+//     let lastGrade = document.forms["AdminForm"]["lastGradeIn"].value;
+//     let grade = document.forms["AdminForm"]["gradeToGo"].value;
+//     let email = document.forms["AdminForm"]["emailIn"].value;
+//     let phoneNumber = document.forms["AdminForm"]["phoneNumberIn"].value;
+//     if(submit = onclick){
+//         if(name == ""){
+//             alert("Kindly Fill in your name");
+//         }
+//         if(age == ""){
+//             alert("Kindly Fill in your age");
+//         }
+//         if(prevSchool == ""){
+//             alert("Kindly Fill in your previous School");
+//         }
+//         if(lastGrade == ""){
+//             alert("Kindly Fill in your Previous grade");
+//         }
+//         if(grade == ""){
+//             alert("Kindly Fill in your current grade");
+        
+//         if(email == ""){
+//             alert("Kindly Fill in your Email");
+//         }
+//         if(phoneNumber == ""){
+//             alert("Kindly Fill in your Phone number");
+//         }
+//         if(phoneNumber > 10){
+//             alert("Your phone number is incorrect");
+//         }
+//         if(phoneNumber < 10){
+//             alert("Your phone number is incorrect");
+//         }
+//     }
+//     }
+//     else{
+//         document.getElementById("message").innerHTML = "Thank you for your time! You will receive a notification after 2 days";
+//     }   
+//     }
+//     form();
+
+// if(sumbit = onclick){
+//     document.getElementById("message").innerHTML = "Thank you for your time! You will receive a notification after 2 days";
+// }
+// if (submitIn==onclick){
+//     document.getElementById("message").innerHTML = "Thank you for your time!";
+//     clea
+// }
